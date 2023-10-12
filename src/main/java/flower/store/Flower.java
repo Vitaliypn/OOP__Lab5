e52759602dd12fa47de759664a10bf8295011f33
Vitaliypn <116552588+Vitaliypn@users.noreply.github.com>
2023-10-12 12:42:04 +0300

    Update Flower.java

--- src/main/java/flower/store/Flower.java
+++ src/main/java/flower/store/Flower.java
@@ -1,6 +1,10 @@
 package flower.store;
 
-import lombok.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
 @Getter @Setter @AllArgsConstructor @ToString @NoArgsConstructor
 public class Flower {
